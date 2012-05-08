63475355b2f3d82ae9f582374b086da161578cea
mallepally85 <mallepally85@gmail.com>
2012-05-08 16:53:04 -0400

    add framework class library

---
+++ Framework/Duck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework
+{
+    public class Duck
+    {
+
+        public void quack()
+        {
+        }
+
+        public void swim()
+        {
+        }
+
+        public void display()
+        {
+        }
+
+        public void fly()
+        {
+        }
+    }
+}
